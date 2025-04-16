e85f9a22fc5e63f6189107c7c1f48deeb672206e
leonburster <leon.burster@icloud.com>
2025-04-15 19:35:17 -0700

    - initial

---
+++ src/JavaFX/Main.java
@@ -0,0 +1,32 @@
+package JavaFX;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Label label = new Label("Hello, JavaFX!");
+
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().add(label);
+
+        Scene scene = new Scene(stackPane, 500,500);
+
+        // Set up and display the stage
+        primaryStage.setTitle("Java FX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
